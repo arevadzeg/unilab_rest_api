515c779acddc8bbedd859320a96047b6b10a40fd
unknown <gioarevadze97@gmail.com>
2021-02-24 12:34:38 +0400

    SECOND TRY

--- API.py
+++ API.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect
 from flask_jwt import  JWT
 from security import authentication, identity
 from resources.iphones import Iphones
@@ -10,12 +10,19 @@ from resources.user import Users, Register
 
 
 
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
 api = Api(app)
 jwt = JWT(app, authentication, identity)
 app.config['PROPAGATE_EXCEPTIONS'] = True
+
+
+@app.route("/")
+def home():
+    return redirect('https://github.com/arevadzeg/unilab_rest_api/tree/main'), 302
 
 @app.before_first_request
 def create_table():
